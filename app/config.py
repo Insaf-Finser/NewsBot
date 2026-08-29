daced2a26b37f761d62c72d901e4aa939c0f24a4
Insaf Finser <insaffinser@gmail.com>
2026-08-29 22:28:23 +0400

    Add Optional Requirments in app/config settings

--- app/config.py
+++ app/config.py
@@ -2,6 +2,9 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    telegram_bot_token: str  = ""
+    telegram_chat_id: str = ""
+    llm_api_key: str = ""
     model_config = SettingsConfigDict(env_file=".env",extra="ignore")
 
 settings = Settings()
