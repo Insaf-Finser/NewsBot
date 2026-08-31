fa2dd1e9991792f6e778c85d49238f0ba5d88375
Insaf Finser <insaffinser@gmail.com>
2026-08-31 12:19:33 +0400

    Add new route test-notifier in main for testing notifier

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.config import settings
+from app.notifiers.telegram import TelegramNotifier
+
+notifier = TelegramNotifier(token=settings.telegram_bot_token)
 
 app = FastAPI()
 
@@ -6,3 +10,9 @@ app = FastAPI()
 @app.get("/health")
 def check_health():
     return {"status":"ok"}
+
+@app.get("/test-notifier")
+async def test_notifier():
+    await notifier.send(chat_id=settings.telegram_chat_id,text="Hello Sir!! Daily news Here.....")
+
+    return {"status":"sent"}
